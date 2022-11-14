345d1978aea40544b9ac9321d98dda5e6cafa7f2
Pi Harshini <yugathesyuga@gmail.com>
2022-11-14 23:24:49 +0800

    Confidence Changed

--- TrainingRecognizer.py
+++ TrainingRecognizer.py
@@ -60,16 +60,17 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 			if value == id_:
 				print("Name : " + name + " --- Confidence : " + str(conf))
 				#Rectangle frame will output
-				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-				cv2.putText(frame, name + str(loss), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
+				if con >= 60:
+					cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+					cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
 
         #Check lossidence, If <70 the doors open
-		if loss <= 70:
+		if loss <= 30:
 			GPIO.output(relay, 1)
 			GPIO.output(green, 1)
 			GPIO.output(red, 0)
 			GPIO.output(buzzer, 1)
-			print("Door Unlock")
+			print("Door Unlock by " + name)
 			print("In 10 seconds Door Lock again")
 			sleep(10)			
 			GPIO.output(relay,0)
